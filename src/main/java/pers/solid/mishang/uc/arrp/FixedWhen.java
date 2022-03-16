78ebbb8142c4071aeb1af8e0512e9b29f40f5bcc
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-03-16 16:43:05 +0800

    加入 ARRPGenerator 未来将会使用； 优化 ARRPMain，更好地使用反射。

--- src/main/java/pers/solid/mishang/uc/arrp/FixedWhen.java
+++ src/main/java/pers/solid/mishang/uc/arrp/FixedWhen.java
@@ -6,7 +6,6 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import net.devtech.arrp.json.blockstate.JWhen;
 import net.minecraft.util.Pair;
-import org.jetbrains.annotations.ApiStatus;
 import pers.solid.mishang.uc.mixin.JWhenAccessor;
 
 import java.lang.reflect.Type;
@@ -15,11 +14,8 @@ import java.util.List;
 
 /**
  * 修复有多个属性时，产生的JSON格式不正确的问题。<br>
- *
- * @deprecated 未来版本的 ARRP 模组将会修复这个问题，届时一段时间后，本模组不再进行本地修复，同时对 ARRP 的版本提出要求。
+ * 未来版本的 ARRP 模组将会修复这个问题，届时一段时间后，本模组不再进行本地修复，同时对 ARRP 的版本提出要求。
  */
-@Deprecated
-@ApiStatus.ScheduledForRemoval(inVersion = "0.1.6")
 public class FixedWhen extends JWhen {
   public static class Serializer implements JsonSerializer<FixedWhen> {
 
