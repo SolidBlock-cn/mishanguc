83c7ac74c73485e893adee0ce67eb1795cb5603c
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-04-04 22:56:35 +0800

    针对服务器与客户端内容再次进行优化，TextClickEvent、NbtClickEvent、NbtPrettyPrinter 现在是仅限客户端的了。 添加了文本复制工具的纹理。 修改了数据标签工具的逻辑，使用客户端与服务器的数据通信，并修复了数据标签工具消息和初始化时的提示消息。

--- src/main/java/pers/solid/mishang/uc/util/TextClickEvent.java
+++ src/main/java/pers/solid/mishang/uc/util/TextClickEvent.java
@@ -1,20 +1,24 @@
 package pers.solid.mishang.uc.util;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * The click event that, when clicked, shows the player another {@link Text}.
+ * <p>The click event that, when clicked, shows the player another {@link Text}.</p>
  *
  * @see pers.solid.mishang.uc.mixin.ScreenMixin#handleTextClickMixin(Style, CallbackInfoReturnable)
+ * @since 0.1.7 This class is designed for client-only, as it is related to client-side clicking actions, and it cannot be serialized as JSON.
  */
+@Environment(EnvType.CLIENT)
 public class TextClickEvent extends ClickEvent {
   public final Text text;
 
   public TextClickEvent(Text text) {
-    super(Action.RUN_COMMAND, "/tell @s INVALID_COMMAND_ACCESS");
+    super(Action.RUN_COMMAND, "");
     this.text = text;
   }
 }
