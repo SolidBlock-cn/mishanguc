d525eaef7c89bc2a1fa530f564b216b43e2ef124
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-05-16 17:55:53 +0800

    - 调整了道路纹理 - 为自动道路方块添加了更复杂的机制 - 道路方块不再允许放置雪 - 移除了不必要的 mixin 类

---
+++ src/main/java/pers/solid/mishang/uc/mixin/SnowBlockMixin.java
@@ -0,0 +1,30 @@
+package pers.solid.mishang.uc.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SnowBlock;
+import net.minecraft.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.WorldView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SnowBlock.class)
+public abstract class SnowBlockMixin {
+  private static final TagKey<Block> ROADS = TagKey.of(Registry.BLOCK_KEY, new Identifier("mishanguc", "roads"));
+
+  /**
+   * 雪不能放置在道路方块上面。
+   */
+  @Inject(method = "canPlaceAt", at = @At("HEAD"), cancellable = true)
+  public void canPlaceAtMixin(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+    if (world.getBlockState(pos.down()).isIn(ROADS)) {
+      cir.setReturnValue(false);
+      cir.cancel();
+    }
+  }
+}
