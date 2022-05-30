0c1d8b8eac950d5c055e09fc498e3d78764188ca
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-05-30 22:07:34 +0800

    合并 1.18.1 分支中的一些差异

--- src/main/java/pers/solid/mishang/uc/blockentity/WallSignBlockEntity.java
+++ src/main/java/pers/solid/mishang/uc/blockentity/WallSignBlockEntity.java
@@ -11,7 +11,6 @@ import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtString;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
-import pers.solid.mishang.uc.block.FullWallSignBlock;
 import pers.solid.mishang.uc.render.WallSignBlockEntityRenderer;
 import pers.solid.mishang.uc.text.TextContext;
 
@@ -76,7 +75,7 @@ public class WallSignBlockEntity extends BlockEntityWithText {
 
   @Override
   public float getHeight() {
-    return getCachedState().getBlock() instanceof FullWallSignBlock ? 16 : 8;
+    return 8;
   }
 
   @Override
