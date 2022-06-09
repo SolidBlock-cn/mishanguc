13c9db27d17f82999b5c062abb173ec4f22f9c46
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-06-09 16:03:57 +0800

    更新版本并修复崩溃

--- src/main/java/pers/solid/mishang/uc/item/TextCopyToolItem.java
+++ src/main/java/pers/solid/mishang/uc/item/TextCopyToolItem.java
@@ -76,13 +76,14 @@ public class TextCopyToolItem extends BlockToolItem implements ItemResourceGener
 
   @Override
   public Text getName(ItemStack stack) {
-    final NbtCompound nbt = stack.getNbt();
+    final NbtCompound nbt = stack.getTag();
     if (nbt == null || !nbt.contains("texts", NbtType.LIST)) return super.getName(stack);
-    final MutableText text = super.getName(stack).shallowCopy();
+    final MutableText text = super.getName(stack).copy();
     final List<MutableText> texts = ImmutableList.copyOf(
         nbt.getList("texts", NbtType.COMPOUND).stream()
             .map(TextContext::fromNbt)
             .map(TextContext::asStyledText)
+            .filter(Objects::nonNull)
             .iterator());
     if (!texts.isEmpty()) {
       MutableText appendable = new LiteralText("");
--- src/main/java/pers/solid/mishang/uc/text/TextContext.java
+++ src/main/java/pers/solid/mishang/uc/text/TextContext.java
@@ -345,7 +345,7 @@ public class TextContext implements Cloneable {
       return;
     }
     cachedText = text;
-    formattedText = text.shallowCopy();
+    formattedText = text.copy();
 
     if (bold) {
       formattedText.formatted(Formatting.BOLD);
