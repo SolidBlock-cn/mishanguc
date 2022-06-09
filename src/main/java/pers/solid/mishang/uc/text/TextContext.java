c36fa3a62fd0e5788df69fb2dfd548c61329b289
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-06-09 16:03:57 +0800

    更新版本并修复崩溃

--- src/main/java/pers/solid/mishang/uc/text/TextContext.java
+++ src/main/java/pers/solid/mishang/uc/text/TextContext.java
@@ -351,7 +351,7 @@ public class TextContext implements Cloneable {
       return;
     }
     cachedText = text;
-    formattedText = text.shallowCopy();
+    formattedText = text.copy();
 
     if (bold) {
       formattedText.formatted(Formatting.BOLD);
