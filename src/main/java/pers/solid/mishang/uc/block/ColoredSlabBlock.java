6b8e5e5c7552245738997c23eaf67ceb55c78fe8
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-08-10 16:18:50 +0800

    - 修复灯光方块纹理问题 - 添加更多染色方块 - 染色工具支持原版方块

--- src/main/java/pers/solid/mishang/uc/block/ColoredSlabBlock.java
+++ src/main/java/pers/solid/mishang/uc/block/ColoredSlabBlock.java
@@ -1,19 +1,19 @@
 package pers.solid.mishang.uc.block;
 
-import net.devtech.arrp.generator.BlockResourceGenerator;
-import net.devtech.arrp.json.blockstate.JBlockStates;
+import net.devtech.arrp.api.RuntimeResourcePack;
+import net.devtech.arrp.generator.BRRPSlabBlock;
 import net.devtech.arrp.json.loot.JLootTable;
 import net.devtech.arrp.json.models.JModel;
-import net.devtech.arrp.json.models.JTextures;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.data.server.BlockLootTableGenerator;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.NotNull;
@@ -22,12 +22,9 @@ import pers.solid.mishang.uc.blockentity.SimpleColoredBlockEntity;
 
 import java.util.List;
 
-public class ColoredGlassBlock extends AbstractGlassBlock implements ColoredBlock, BlockResourceGenerator {
-  private final JTextures textures;
-
-  public ColoredGlassBlock(Settings settings, JTextures textures) {
-    super(settings);
-    this.textures = textures;
+public class ColoredSlabBlock extends BRRPSlabBlock implements ColoredBlock {
+  public ColoredSlabBlock(@Nullable Block baseBlock, Settings settings) {
+    super(baseBlock, settings);
   }
 
   @Override
@@ -50,17 +47,24 @@ public class ColoredGlassBlock extends AbstractGlassBlock implements ColoredBloc
   @Environment(EnvType.CLIENT)
   @Override
   public @NotNull JModel getBlockModel() {
-    return new JModel("mishanguc:block/colored_cube_all").textures(textures);
+    return super.getBlockModel().parent(new Identifier("mishanguc", "block/colored_slab"));
   }
+
 
   @Environment(EnvType.CLIENT)
   @Override
-  public @NotNull JBlockStates getBlockStates() {
-    return JBlockStates.simple(getBlockModelId());
+  public void writeBlockModel(RuntimeResourcePack pack) {
+    final JModel model = getBlockModel();
+    final Identifier id = getBlockModelId();
+    pack.addModel(model, id);
+    pack.addModel(model.clone().parent(new Identifier("mishanguc", "block/colored_slab_top")), id.brrp_append("_top"));
+    if (baseBlock == null) {
+      pack.addModel(model.clone().parent(new Identifier("mishanguc", "block/colored_cube_bottom_up")), id.brrp_append("_double"));
+    }
   }
 
   @Override
   public JLootTable getLootTable() {
-    return JLootTable.delegate(BlockLootTableGenerator.drops(this).apply(COPY_COLOR_LOOT_FUNCTION).build());
+    return JLootTable.delegate(BlockLootTableGenerator.slabDrops(this).apply(COPY_COLOR_LOOT_FUNCTION).build());
   }
 }
