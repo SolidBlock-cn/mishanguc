628970af3b305968bc6219314ddb08ac00aa376f
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2022-12-03 10:00:14 +0800

    修复地图显示与方块剔除有关的问题

--- src/main/java/pers/solid/mishang/uc/blockentity/ColoredBlockEntity.java
+++ src/main/java/pers/solid/mishang/uc/blockentity/ColoredBlockEntity.java
@@ -10,6 +10,7 @@ public interface ColoredBlockEntity {
     MapColor nearestMapColor = MapColor.CLEAR;
     for (int i = 0; i < 64; i++) {
       final MapColor mapColor1 = MapColor.get(i);
+      if (mapColor1 == null) continue;
       final int mapColor = mapColor1.color;
       final int distance = Math.abs((mapColor << 4) % 255 - (color << 4) % 255) + Math.abs((mapColor << 2) % 255 - (color << 2) % 255) + Math.abs(mapColor % 255 - color % 255);
       if (distance < lastDistance) {
