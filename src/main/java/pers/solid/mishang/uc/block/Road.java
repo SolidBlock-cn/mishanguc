1f3225979b3fe3245dae4985438716103e303943
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2023-12-15 19:06:13 +0800

    更新至 1.2.7

--- src/main/java/pers/solid/mishang/uc/block/Road.java
+++ src/main/java/pers/solid/mishang/uc/block/Road.java
@@ -3,18 +3,15 @@ package pers.solid.mishang.uc.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.entity.attribute.EntityAttributeInstance;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.registry.tag.TagKey;
 import net.minecraft.state.StateManager;
 import net.minecraft.text.Text;
-import net.minecraft.util.*;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -24,75 +21,17 @@ import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 import pers.solid.brrp.v1.generator.BlockResourceGenerator;
-import pers.solid.mishang.uc.MishangucRules;
 import pers.solid.mishang.uc.util.EightHorizontalDirection;
 import pers.solid.mishang.uc.util.LineColor;
 import pers.solid.mishang.uc.util.LineType;
 import pers.solid.mishang.uc.util.RoadConnectionState;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.function.Consumer;
 
 /**
  * 所有道路方块类型均实现的接口。接口可以多重继承，并直接实现于已有类上，因此使用接口。
  */
 public interface Road extends BlockResourceGenerator {
-
-  EntityAttributeModifier ROAD_SPEED_BOOST = new EntityAttributeModifier(UUID.fromString("693D7032-4767-5A57-A28F-401F8F485772"/* 根据网上的在线 UUID 生成器生成 */), "road_speed_boost", 1.75, EntityAttributeModifier.Operation.MULTIPLY_TOTAL) {
-    @Override
-    public double getValue() {
-      return MishangucRules.currentRoadBoostSpeed;
-    }
-
-    @Override
-    public NbtCompound toNbt() {
-      final NbtCompound nbt = super.toNbt();
-      nbt.putDouble("Amount", getValue());
-      return nbt;
-    }
-
-    /**
-     * 由于超类方法中要求是同一类别才能相等，匿名类的使用影响了相等的判断，故这里临时做出修改。
-     */
-    @Override
-    public boolean equals(Object o) {
-      return super.equals(o) || (o instanceof EntityAttributeModifier entityAttributeModifier && Objects.equals(entityAttributeModifier.getId(), this.getId()));
-    }
-  };
-  TagKey<Block> ROADS = TagKey.of(RegistryKeys.BLOCK, new Identifier("mishanguc", "roads"));
-
-  /**
-   * 当玩家踩踏在道路方块上时，给予对应的速度倍率值。踩踏在其他方块上时，该倍率值被移除。特别注意，当玩家在道路方块之间上下楼梯的时候，会存在没有踩踏在道路方块上的短暂期间，这种情况下不应该移除其倍率值。<p>
-   * 该方法在客户端和服务器的每一刻都会执行。
-   * <p>
-   * 该方法的逻辑如下：
-   * <ul>
-   *   <li>当玩家踩在道路方块上时，一定给予效果。</li>
-   *   <li>当玩家下方的方块为无碰撞箱的方块且该方块下方为道路方块时，一定不作处理。</li>
-   *   <li>不符合上述两条条件，则立即移除效果。</li>
-   *   </ul>
-   */
-  Consumer<World> CHECK_MULTIPLIER = world -> {
-    for (PlayerEntity player : world.getPlayers()) {
-      final EntityAttributeInstance attributeInstance = player.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
-      final BlockPos stepPos = new BlockPos(player.getPos().x, player.getPos().y - 0.2, player.getPos().z);
-      final BlockPos stepPosDown = stepPos.down();
-      if (attributeInstance == null) continue;
-      final BlockState stepState = world.getBlockState(stepPos);
-      if (player.isOnGround() && stepState.isIn(ROADS)) {
-        // 玩家踩在道路方块上。
-        if (!attributeInstance.hasModifier(ROAD_SPEED_BOOST)) {
-          attributeInstance.addTemporaryModifier(ROAD_SPEED_BOOST);
-        }
-      } else if ((!stepState.getCollisionShape(world, stepPos).isEmpty() || !world.getBlockState(stepPosDown).isIn(ROADS)) && !stepState.isIn(ROADS)) {
-        if (attributeInstance.hasModifier(ROAD_SPEED_BOOST)) {
-          attributeInstance.removeModifier(ROAD_SPEED_BOOST);
-        }
-      }
-    }
-  };
 
   /**
    * 获取该方块状态中，某个特定方向上的连接状态。连接状态可用于自动路块。
