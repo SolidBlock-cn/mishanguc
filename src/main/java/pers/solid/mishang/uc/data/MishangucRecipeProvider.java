e88f7b6cac46f8cfae31165c5aaa92bfa7c3c2d6
SolidBlock <hulunabc@163.com>
2024-10-23 21:58:27 +0800

    更新至 1.19.4，修复道路标线配方缺失的问题

--- src/main/java/pers/solid/mishang/uc/data/MishangucRecipeProvider.java
+++ src/main/java/pers/solid/mishang/uc/data/MishangucRecipeProvider.java
@@ -47,16 +47,7 @@ public class MishangucRecipeProvider extends FabricRecipeProvider {
   private static void addRegularRecipes(RecipeExporter exporter) {
     for (Block block : MishangUtils.blocks()) {
       if (block instanceof MishangucBlock r) {
-        final CraftingRecipeJsonBuilder craftingRecipe = r.getCraftingRecipe();
-        if (craftingRecipe != null) {
-          craftingRecipe.offerTo(exporter);
-        }
-        if (r.shouldWriteStonecuttingRecipe()) {
-          final SingleItemRecipeJsonBuilder stonecuttingRecipe = r.getStonecuttingRecipe();
-          if (stonecuttingRecipe != null) {
-            stonecuttingRecipe.offerTo(exporter, r.getStonecuttingRecipeId());
-          }
-        }
+        r.writeRecipes(exporter);
       } else {
         throw new IllegalStateException();
       }
