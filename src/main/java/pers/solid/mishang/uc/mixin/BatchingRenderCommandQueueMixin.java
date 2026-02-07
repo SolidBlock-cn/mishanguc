78e2d72dc7197392689a7a81bb188378eec4d7f7
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-02-07 10:24:03 +0800

    仅限 1.21.10 以上版本，在渲染文本时添加一个空白命令，以使得特殊文本能被 Iris 作为文本渲染（否则在光影下，当没有渲染普通文本时，特殊文本的渲染会比较暗淡）

--- src/main/java/pers/solid/mishang/uc/mixin/BatchingRenderCommandQueueMixin.java
+++ src/main/java/pers/solid/mishang/uc/mixin/BatchingRenderCommandQueueMixin.java
@@ -2,8 +2,10 @@ package pers.solid.mishang.uc.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.command.BatchingRenderCommandQueue;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.OrderedText;
 import org.joml.Matrix4f;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -28,6 +30,10 @@ import java.util.List;
 public abstract class BatchingRenderCommandQueueMixin implements RenderCommandQueueExtension, BatchingRenderCommandQueueExtension {
   @Shadow
   private boolean hasCommands;
+
+  @Shadow
+  public abstract void submitText(MatrixStack matrices, float x, float y, OrderedText text, boolean dropShadow, TextRenderer.TextLayerType layerType, int light, int color, int backgroundColor, int outlineColor);
+
   /**
    * 用于渲染 {@link SpecialDrawable} 的命令列表，会在 {@link net.minecraft.client.render.command.TextCommandRenderer TextCommandRenderer} 中执行。
    *
@@ -39,6 +45,7 @@ public abstract class BatchingRenderCommandQueueMixin implements RenderCommandQu
   @Override
   public void submitSpecialDrawable$mishang(MatrixStack matrixStack, SpecialDrawable specialDrawable, int light, float x, float y) {
     this.hasCommands = true;
+    submitText(matrixStack, x, y, OrderedText.EMPTY, false, TextRenderer.TextLayerType.NORMAL, light, 0, 0, 0);
     specialDrawableCommands.add(new SpecialDrawableCommand(new Matrix4f(matrixStack.peek().getPositionMatrix()), specialDrawable, light, x, y));
   }
 
