90deb9b1602fabdb48ef6dbf760e0a05c877b52f
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-02-16 16:05:55 +0800

    更新至 1.6.0-beta.6，现在在告示牌编辑界面中输入特殊文本时，如果输入的内容无效，除了将文本框显示为红色之外，还会显示一个提示以表明错误。

--- src/main/java/pers/solid/mishang/uc/text/SpecialDrawableTypes.java
+++ src/main/java/pers/solid/mishang/uc/text/SpecialDrawableTypes.java
@@ -1,14 +1,16 @@
 package pers.solid.mishang.uc.text;
 
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.InvalidIdentifierException;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import pers.solid.mishang.uc.Mishanguc;
-
-import java.util.function.BiFunction;
+import pers.solid.mishang.uc.util.TextBridge;
 
 public final class SpecialDrawableTypes {
   private SpecialDrawableTypes() {
@@ -20,8 +22,8 @@ public final class SpecialDrawableTypes {
 
   public static final SpecialDrawableType<PatternSpecialDrawable> PATTERN = register("pattern", PatternSpecialDrawable::fromNbt, (textContext, args) -> {
     final PatternSpecialDrawable pattern = PatternSpecialDrawable.fromName(textContext, args);
-    if (pattern.isEmpty()) {
-      return null;
+    if (pattern == null) {
+      throw new CommandSyntaxException(null, TextBridge.translatable("special_drawable.pattern.invalid_name", args));
     } else {
       return pattern;
     }
@@ -31,37 +33,55 @@ public final class SpecialDrawableTypes {
     final Identifier texture = Identifier.tryParse(nbt.getString("texture", null));
     return texture != null && TextureSpecialDrawable.isValidIdentifier(texture) ? new TextureSpecialDrawable(texture, textContext) : null;
   }, (textContext, args) -> {
-    final Identifier identifier = Identifier.tryParse(args);
-    if (identifier == null) {
-      return null;
-    } else if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-      return TextureSpecialDrawable.isValidIdentifier(identifier) ? new TextureSpecialDrawable(identifier, textContext) : null;
+    final Identifier identifier;
+    try {
+      identifier = Identifier.of(args);
+    } catch (InvalidIdentifierException e) {
+      throw Identifier.COMMAND_EXCEPTION.create();
     }
-    return null;
+    if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+      try {
+        TextureSpecialDrawable.validateIdentifier(identifier);
+      } catch (IllegalArgumentException e) {
+        throw new CommandSyntaxException(null, TextBridge.literal(e.getMessage()));
+      }
+      return new TextureSpecialDrawable(identifier, textContext);
+    }
+    throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherUnknownArgument().create();
   });
 
   private static <T extends SpecialDrawableType<? extends SpecialDrawable>> T register(String namePath, T specialDrawable) {
     return Registry.register(SpecialDrawableType.REGISTRY, Mishanguc.id(namePath), specialDrawable);
   }
 
-  private static <S extends SpecialDrawable> SpecialDrawableType<S> register(String namePath, BiFunction<TextContext, NbtCompound, S> fromNbt, BiFunction<TextContext, String, S> fromStringArgs) {
+  private static <S extends SpecialDrawable> SpecialDrawableType<S> register(String namePath, FromNbt<S> fromNbt, FromStringArgs<S> fromStringArgs) {
     return register(namePath, new Simple<>(fromNbt, fromStringArgs));
   }
 
-  private record Simple<S extends SpecialDrawable>(BiFunction<TextContext, NbtCompound, S> fromNbt, BiFunction<TextContext, String, S> fromStringArgs) implements SpecialDrawableType<S> {
+  private record Simple<S extends SpecialDrawable>(FromNbt<S> fromNbt, FromStringArgs<S> fromStringArgs) implements SpecialDrawableType<S> {
 
     @Override
-    public S fromNbt(TextContext textContext, @NotNull NbtCompound nbt) {
-      return fromNbt.apply(textContext, nbt);
+    public @Nullable S fromNbt(@NotNull TextContext textContext, @NotNull NbtCompound nbt) {
+      return fromNbt.fromNbt(textContext, nbt);
     }
 
     @Override
-    public S fromStringArgs(TextContext textContext, String args) {
-      return fromStringArgs.apply(textContext, args);
+    public @NotNull S fromStringArgs(@NotNull TextContext textContext, @NotNull String args) throws CommandSyntaxException {
+      return fromStringArgs.fromStringArgs(textContext, args);
     }
   }
 
   @SuppressWarnings("EmptyMethod")
   public static void init() {
+  }
+
+  @FunctionalInterface
+  public interface FromNbt<S extends SpecialDrawable> {
+    @Nullable S fromNbt(@NotNull TextContext textContext, @NotNull NbtCompound nbt);
+  }
+
+  @FunctionalInterface
+  public interface FromStringArgs<S> {
+    @NotNull S fromStringArgs(@NotNull TextContext textContext, @NotNull String args) throws CommandSyntaxException;
   }
 }
