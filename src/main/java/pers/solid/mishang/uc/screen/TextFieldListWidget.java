ce8b1831fc3c3ddec3595f6ea6438e5b4587d514
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-02-20 16:38:39 +0800

    进一步改善告示牌编辑界面的操作，现在重新调整大小后，文本框和预设列表均保留原先的聚焦；文本框会根据是否存在滚动条自动决定宽度

--- src/main/java/pers/solid/mishang/uc/screen/TextFieldListWidget.java
+++ src/main/java/pers/solid/mishang/uc/screen/TextFieldListWidget.java
@@ -226,6 +226,19 @@ public class TextFieldListWidget extends AlwaysSelectedEntryListWidget<TextField
     return width;
   }
 
+  /**
+   * 在屏幕尺寸改变时，其文本框的宽度也应当同步改变。
+   */
+  @Override
+  public void setWidth(int width) {
+    super.setWidth(width);
+    final boolean scrollbarVisible = overflows();
+    final int elementWidth = width - (scrollbarVisible ? 10 : 4);
+    for (Entry child : children()) {
+      child.textFieldWidget.setWidth(elementWidth);
+    }
+  }
+
   @Override
   public int getRowTop(int index) {
     return super.getRowTop(index) - 2;
@@ -311,6 +324,7 @@ public class TextFieldListWidget extends AlwaysSelectedEntryListWidget<TextField
       rawChildren.add(index, newEntry);
     }
     setScrollY(getScrollY()); // 此处会调用私有方法 recalculateAllChildrenPositions
+    setWidth(getWidth()); // 重新设置其宽度
 
     signBlockEditScreen.updateContentVisibility();
 
@@ -406,6 +420,7 @@ public class TextFieldListWidget extends AlwaysSelectedEntryListWidget<TextField
     removedEntry.setSelected(false);
     // 删除一行元素后，对滚动数量进行一次 clamp，以避免出现过度滚动的情况。
     setScrollY(getScrollY()); // 此处会调用私有方法 recalculateAllChildrenPositions
+    setWidth(getWidth()); // 重新设置其宽度
 
     signBlockEditScreen.updateContentVisibility();
     signBlockEditScreen.changed = true;
