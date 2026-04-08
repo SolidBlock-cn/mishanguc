2e1a65ae0e260130d53c51fc8c3425912a1f793b
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-04-08 12:19:52 +0800

    修复兴趣点类型不支持染色的下界传送门方块，导致通过染色的下界传送门进入并返回后会生成新的未染色的下界传送门的问题。 实现方式：不再修改原版兴趣点，而是注册了模组中的兴趣点，让 Minecraft 在识别传送门时，同时支持原版兴趣点和模组兴趣点。 注：如修改原版兴趣点，会存在 id 为 -1 的问题，原因未知，可能是因为注册兴趣点时还未注册模组中的方块。

--- src/main/java/pers/solid/mishang/uc/mixin/PointOfInterestTypesMixin.java
+++
@@ -1,9 +0,0 @@
-package pers.solid.mishang.uc.mixin;
-
-import net.minecraft.world.poi.PointOfInterestTypes;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(PointOfInterestTypes.class)
-public abstract class PointOfInterestTypesMixin {
-  // 重新实现，但是不能在此时就引用 colored blocks
-}
