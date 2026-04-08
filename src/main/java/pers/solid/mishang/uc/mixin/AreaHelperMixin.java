5d77fa29b57b8cad02a6e8fdf7c8b4a1c020031b
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-04-08 12:19:52 +0800

    修复兴趣点类型不支持染色的下界传送门方块，导致通过染色的下界传送门进入并返回后会生成新的未染色的下界传送门的问题。 实现方式：不再修改原版兴趣点，而是注册了模组中的兴趣点，让 Minecraft 在识别传送门时，同时支持原版兴趣点和模组兴趣点。 注：如修改原版兴趣点，会存在 id 为 -1 的问题，原因未知，可能是因为注册兴趣点时还未注册模组中的方块。

--- src/main/java/pers/solid/mishang/uc/mixin/AreaHelperMixin.java
+++ src/main/java/pers/solid/mishang/uc/mixin/AreaHelperMixin.java
@@ -1,22 +1,19 @@
 package pers.solid.mishang.uc.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
 import net.minecraft.world.dimension.NetherPortal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import pers.solid.mishang.uc.blocks.ColoredBlocks;
 
 @Mixin(NetherPortal.class)
 public abstract class AreaHelperMixin {
-  @Inject(method = "validStateInsidePortal", at = @At("RETURN"), cancellable = true)
-  private static void validColoredPortal(BlockState state, CallbackInfoReturnable<Boolean> cir) {
-    if (state.isOf(ColoredBlocks.COLORED_NETHER_PORTAL)) {
-      cir.setReturnValue(true);
-    }
+  @ModifyReturnValue(method = "validStateInsidePortal", at = @At("RETURN"))
+  private static boolean validColoredPortal(boolean original, @Local(argsOnly = true, name = "state", ordinal = 0) BlockState state) {
+    return original || state.isOf(ColoredBlocks.COLORED_NETHER_PORTAL);
   }
 
   @ModifyExpressionValue(method = "getPotentialHeight", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"))
