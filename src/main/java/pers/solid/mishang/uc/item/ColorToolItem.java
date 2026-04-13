d17c748c78dce36f08bfca27fe5b40a997b50ef9
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-04-13 11:12:09 +0800

    修复文本复制工具在使用时可能会导致使用副手物品的问题，更新至 1.6.3-beta.2 优化双持操作，在使用生长工具、搬运工具、瞬移工具和颜色工具时，即使操作失败，也不会使用副手物品。

--- src/main/java/pers/solid/mishang/uc/item/ColorToolItem.java
+++ src/main/java/pers/solid/mishang/uc/item/ColorToolItem.java
@@ -111,7 +111,7 @@ public class ColorToolItem extends BlockToolItem implements MishangucItem, WithM
         player.sendMessage(TextBridge.translatable("item.mishanguc.color_tool.message.no_data").formatted(Formatting.RED), true);
         return ActionResult.FAIL;
       }
-      return ActionResult.PASS;
+      return ActionResult.CONSUME;
     }
 
     int prevColorRgb = 0; // the initial value should not usually be used.
@@ -180,7 +180,7 @@ public class ColorToolItem extends BlockToolItem implements MishangucItem, WithM
         player.sendMessage(TextBridge.translatable("item.mishanguc.color_tool.message.not_colored").formatted(Formatting.RED), true);
         return ActionResult.FAIL;
       }
-      return ActionResult.PASS;
+      return ActionResult.CONSUME;
     }
   }
 
