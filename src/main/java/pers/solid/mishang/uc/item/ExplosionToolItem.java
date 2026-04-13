8e2962a9201b972fb0f3fe959ce9da7c2aec86c8
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-04-13 16:25:39 +0800

    修复爆炸工具在使用时可能会导致使用副手物品的问题。

--- src/main/java/pers/solid/mishang/uc/item/ExplosionToolItem.java
+++ src/main/java/pers/solid/mishang/uc/item/ExplosionToolItem.java
@@ -49,7 +49,7 @@ public class ExplosionToolItem extends Item implements HotbarScrollInteraction, 
       return ActionResult.FAIL;
     }
     if (!(world instanceof ServerWorld serverWorld)) {
-      return ActionResult.PASS;
+      return ActionResult.CONSUME;
     }
     if (!serverWorld.getGameRules().get(MishangucRules.EXPLOSION_TOOL_ACCESS).get().hasAccess(user, true)) {
       return ActionResult.PASS;
