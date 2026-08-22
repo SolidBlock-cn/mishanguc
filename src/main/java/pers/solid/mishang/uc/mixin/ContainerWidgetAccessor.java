281f78914ea234fbdbeef2efc4bc45ffeff38955
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-08-22 23:14:01 +0800

    个别格式调整

--- src/main/java/pers/solid/mishang/uc/mixin/ContainerWidgetAccessor.java
+++ src/main/java/pers/solid/mishang/uc/mixin/ContainerWidgetAccessor.java
@@ -9,6 +9,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(ContainerWidget.class)
 public interface ContainerWidgetAccessor {
   @Accessor("focusedElement")
-  // 方法不命名为 setFocused，避免与原版方法冲突，导致通过原版的 setFocused 方法修改了 selected
+    // 方法不命名为 setFocused，避免与原版方法冲突，导致通过原版的 setFocused 方法修改了 selected
   void setFocusedRaw(@Nullable Element focusedElement);
 }
--- src/main/java/pers/solid/mishang/uc/screen/AbstractSignBlockEditScreen.java
+++ src/main/java/pers/solid/mishang/uc/screen/AbstractSignBlockEditScreen.java
@@ -108,14 +108,14 @@ public abstract class AbstractSignBlockEditScreen<T extends BlockEntityWithText>
   public final ButtonWidget addTextButton = new ButtonWidget.Builder(TextBridge.translatable("message.mishanguc.add_text"), button1 -> {
     if (textFieldListWidget.selectedEntries.isEmpty()) {
       final TextFieldListWidget.Entry newEntry = textFieldListWidget.addEmptyTextField(-1);
-        textFieldListWidget.setFocusedAndSelected(newEntry, false, false);
+      textFieldListWidget.setFocusedAndSelected(newEntry, false, false);
     } else {
       final List<TextFieldListWidget.Entry> selectedCopy = Lists.reverse(textFieldListWidget.children()).stream().filter(textFieldListWidget.selectedEntries::contains).toList();
       final TextFieldListWidget.Entry previouslySelected = textFieldListWidget.getSelectedOrNull();
       for (TextFieldListWidget.Entry selectedEntry : selectedCopy) {
         selectedEntry.setSelected(false);
       }
-        textFieldListWidget.setFocusedAndSelected(null, false, false);
+      textFieldListWidget.setFocusedAndSelected(null, false, false);
       for (TextFieldListWidget.Entry entry : selectedCopy) {
         final int i = textFieldListWidget.children().indexOf(entry);
         if (i < 0) {
@@ -124,7 +124,7 @@ public abstract class AbstractSignBlockEditScreen<T extends BlockEntityWithText>
         }
         final TextFieldListWidget.Entry newEntry = textFieldListWidget.addEmptyTextField(i + 1);
         if (entry == previouslySelected) {
-            textFieldListWidget.setFocusedAndSelected(newEntry, true, false);
+          textFieldListWidget.setFocusedAndSelected(newEntry, true, false);
         } else {
           newEntry.setSelected(true);
         }
@@ -155,7 +155,7 @@ public abstract class AbstractSignBlockEditScreen<T extends BlockEntityWithText>
         if (!textFieldListWidget.children().isEmpty()) {
           final TextFieldListWidget.Entry nearbyEntry = textFieldListWidget.children().get(MathHelper.clamp(index - 1, 0, children().size() - 1));
           if (entry == previouslySelected) {
-              textFieldListWidget.setFocusedAndSelected(nearbyEntry, true, false);
+            textFieldListWidget.setFocusedAndSelected(nearbyEntry, true, false);
           }
           nearbyEntry.setSelected(true);
         }
