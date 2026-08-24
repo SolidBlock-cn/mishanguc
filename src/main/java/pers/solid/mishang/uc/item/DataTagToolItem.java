5c048baa731d3eca50e4ca016468ef083d16b81f
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-08-24 23:12:42 +0800

    同步 26.2 的修改，优化实体渲染的方式，在渲染实体轮廓时，现在会考虑 partialTicks，使实体运动时轮廓更流畅（对其他类似工具也一并做了此更改）

--- src/main/java/pers/solid/mishang/uc/item/DataTagToolItem.java
+++ src/main/java/pers/solid/mishang/uc/item/DataTagToolItem.java
@@ -10,10 +10,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.VertexRendering;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.command.BlockDataObject;
 import net.minecraft.command.EntityDataObject;
@@ -39,6 +36,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
+import net.minecraft.world.tick.TickManager;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -135,7 +133,10 @@ public class DataTagToolItem extends BlockToolItem implements InteractsWithEntit
       if (consumers == null) return;
       final VertexConsumer vertexConsumer = consumers.getBuffer(RenderLayer.getLines());
       final Vec3d cameraPos = context.camera().getPos();
-      VertexRendering.drawOutline(matrices, vertexConsumer, VoxelShapes.cuboid(entity.getBoundingBox()), -cameraPos.x, -cameraPos.y, -cameraPos.z, ColorHelper.fromFloats(0.8f, 0f, 1f, 0f));
+      final RenderTickCounter deltaTracker = MinecraftClient.getInstance().getRenderTickCounter();
+      final TickManager tickRateManager = context.world().getTickManager();
+      final float tickProgress = deltaTracker.getTickProgress(!tickRateManager.shouldSkipTick(entity));
+      VertexRendering.drawOutline(matrices, vertexConsumer, VoxelShapes.cuboid(entity.getBoundingBox().offset(entity.getLerpedPos(tickProgress).subtract(entity.getPos()))), -cameraPos.x, -cameraPos.y, -cameraPos.z, ColorHelper.fromFloats(0.8f, 0f, 1f, 0f));
     }
   }
 
