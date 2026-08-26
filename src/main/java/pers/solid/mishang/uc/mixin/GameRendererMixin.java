29f786a0efe45b53d44fbf981f4ce772b82bcd7b
SolidBlock <32444848+SolidBlock-cn@users.noreply.github.com>
2026-08-26 21:09:39 +0800

    修复在 NeoForge 和 Sinytra Connector 中启动失败的问题 （注：即使在 `method` 中指定完整的方法签名，经 Yarn 重新映射后的中间名仅有方法名称，中间名的方法名称一般没有问题，但是经 Sinytra Connector 重映射为官方映射后的方法名称 `pick` 存在歧义，无法正确定位目标）

--- src/main/java/pers/solid/mishang/uc/mixin/GameRendererMixin.java
+++ src/main/java/pers/solid/mishang/uc/mixin/GameRendererMixin.java
@@ -3,13 +3,16 @@ package pers.solid.mishang.uc.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.HitResult;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Desc;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import pers.solid.mishang.uc.item.BlockToolItem;
 
@@ -21,13 +24,13 @@ public class GameRendererMixin {
 
   @ModifyArg(
       method = "findCrosshairTarget(Lnet/minecraft/entity/Entity;DDF)Lnet/minecraft/util/hit/HitResult;",
+      target = @Desc(id = "findCrosshairTarget_yarn", value = "findCrosshairTarget", args = {Entity.class, double.class, double.class, float.class}, ret = HitResult.class),
       at =
       @At(
           value = "INVOKE",
           target = "Lnet/minecraft/entity/Entity;raycast(DFZ)Lnet/minecraft/util/hit/HitResult;"),
       index = 2)
   private boolean modifyRaycastCall(boolean includeFluids) {
-    //        return true;
     final ClientPlayerEntity player = this.client.player;
     if (player == null) {
       return includeFluids;
